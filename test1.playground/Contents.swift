d9a701fbb80b36468142c408c7580870258c6513
Bryan Grill <grillb@gmail.com>
2020-01-13 19:48:47 -0500

    test xcode github integration

---
+++ test1.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+var bookPrice = 39
+var numOfCopies = 5
+var totalPrice = bookPrice * numOfCopies
+var totalPriceMessage = "The price of the book is $\(totalPrice)"
+
+// https://www.appcoda.com/learnswift/playgrounds.html
+// at Control Flow Basics
+// new comment
